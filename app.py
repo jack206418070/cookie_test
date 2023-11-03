b3973b673f42293a4d634563e4ab5d40910e0f15
GinJack <jack90928@gmail.com>
2023-11-03 22:05:51 +0800

    first push

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route('/set_cookie')
+def index():
+  cookie_value = request.cookies.get('sessionid', None)
+  if cookie_value:
+      # 如果cookie存在，执行相关操作
+      pass
+  return render_template('index.html', cookie_value=cookie_value)
+app.run(port=5542)
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, make_response
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route('/set_cookie')
+def set_cookie():
+    resp = make_response("Cookie Set")
+    resp.set_cookie('sessionid', 'cookie_value', domain='.domain.com')
+    return resp
+
+
+app.run(port=5541)
