8624ddbbbb705d3019f93c8ed9984b6b0e2ef013
GinJack <jack90928@gmail.com>
2023-11-03 22:21:14 +0800

    udpate

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app.secret_key = 'your_secret_key'
 @app.route('/set_cookie')
 def index():
   cookie_value = request.cookies.get('sessionid', None)
+  print(cookie_value)
   if cookie_value:
       # 如果cookie存在，执行相关操作
       pass
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.secret_key = 'your_secret_key'
 @app.route('/set_cookie')
 def set_cookie():
     resp = make_response("Cookie Set")
-    resp.set_cookie('sessionid', 'cookie_value', domain='.domain.com', secure=False)
+    resp.set_cookie('sessionid', 'cookie_value', domain='.shohumask.shop', secure=False)
     return resp
 
 
