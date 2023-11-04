7a812d2dcebb6e1ec9d63c8fe8560622571897e2
GinJack <jack90928@gmail.com>
2023-11-05 00:24:06 +0800

    update

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, redirect
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 @app.route('/set_cookie')
 def set_cookie():
-    resp = make_response("Cookie Set")
+    resp = make_response(redirect('http://sub.shohumask.shop/'))
     resp.set_cookie('sessionid', 'cookie_value', domain='.shohumask.shop', secure=False)
     return resp
 
