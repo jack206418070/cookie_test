6e4329681858e25286edf0721d60c77ab2f2dfbb
GinJack <jack90928@gmail.com>
2023-11-05 00:25:32 +0800

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app.secret_key = 'your_secret_key'
 
 @app.route('/set_cookie')
 def set_cookie():
-    resp = make_response(redirect('http://sub.shohumask.shop/'))
+    resp = make_response(redirect('http://sub.shohumask.shop/set_cookie'))
     resp.set_cookie('sessionid', 'cookie_value', domain='.shohumask.shop', secure=False)
     return resp
 
